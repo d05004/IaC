da74b69872bd961585136635ba6f98ee18fe0ac5
d05004 <ded05004@naver.com>
2023-04-17 20:10:28 +0900

    dns update

--- test.sh
+++ test.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 echo "script start" >> /tmp/result
+echo "nameserver 8.8.8.8" >> /etc/resolv.conf
+echo "nameserver 8.8.4.4" >> /etc/resolv.conf
+echo "nameserver 8.8.8.8 8.8.4.4" >> /etc/network/interfaces
+
 apt-get update -y
 apt-get install apache2
 echo "script end" >> /tmp/result
